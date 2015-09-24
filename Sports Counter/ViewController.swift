bb6167c14b16b434b39b1d920c539434458d2ca9
Rosario Tarabocchia <rtarabocchia@gmail.com>
2015-09-24 16:54:19 -0400

    Initial Commit

--- Sports Counter/ViewController.swift
+++ Sports Counter/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var btnScore0: UIButton!
+    @IBOutlet weak var btnScore1: UIButton!
+    @IBOutlet weak var btnReset: UIButton!
+    
+    var score0 : Int = 0
+    var score1 : Int = 0
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +31,62 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    @IBAction func btnScore0ACTION(sender: UIButton) {
+        
+        addScore0()
+    }
+    
+    
+    @IBAction func btnScore1ACTION(sender: UIButton) {
+        
+        addScore1()
+    }
+    
+    
+    @IBAction func btnResetACTION(sender: UIButton) {
+        
+        reset()
+    }
+    
+    func addScore0(){
+        
+        score0 = score0 + 1
+        print()
+        
+    }
+    
+    func addScore1(){
+        
+        score1 = score1 + 1
+        print()
+        
+        
+    }
+    
+    func reset(){
+        score0 = 0
+        score1 = 0
+        print()
+        
+        
+        
+        
+    }
+    
+    func print(){
+        
+        btnScore1.setTitle("\(score1)", forState: UIControlState.Normal)
+        btnScore0.setTitle("\(score0)", forState: UIControlState.Normal)
+        
+        
+    }
+    
+    
+    
+    
 
 
 }
